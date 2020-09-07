56db3da5b0286feedf5be008dceec639ad3c97f6
Abdullah Diaa <abdullah.diaa@gmail.com>
2020-09-07 11:22:36 +0300

    Add tests placeholder

--- test/pushbots_flutter_test.dart
+++ test/pushbots_flutter_test.dart
@@ -3,21 +3,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pushbots_flutter/pushbots_flutter.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('pushbots_flutter');
-
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
+  group('Sample test', () {
+    test('value should start at 0', () {
+      expect(0, 0);
     });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-
   });
 }
